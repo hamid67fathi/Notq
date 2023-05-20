4af737407b90ad0297b1c3b811a2000874f72b27
Hamid Fathi <hamid67fathi@hotmail.com>
2023-05-20 15:44:22 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     install_requires=['torchaudio ==2.0.2',
                         'pydub ==0.25.1',
                         'speechRecognition ==3.8.1',
-                        'numpy ==1.19.2',
+                        'numpy ==1.24.3',
                         'tqdm ==4.61.2',
                         'transformers ==4.11.2',
                         'torch ==2.0.1',
